cd9b1499eae7c64363a306e838cb8f5e2a4afd26
amit17rajput <amit17rajput@gmail.com>
2021-09-19 20:11:12 +0530

    initial commit

---
+++ Config.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Twilio.logic
+{
+   public static class Config
+    {
+        public static string accountid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        public static string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+
+
+
+        //// Find your Account SID and Auth Token at twilio.com/console
+        //// and set the environment variables. See http://twil.io/secure
+        //string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        //string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        //TwilioClient.Init(accountSid, authToken);
+
+        //var message = MessageResource.Create(
+        //    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+        //    from: new Twilio.Types.PhoneNumber("+15017122661"),
+        //    to: new Twilio.Types.PhoneNumber("+15558675310")
+        //);
+
+        //Console.WriteLine(message.Sid);
+
+    }
+}
---
+++ Request.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Twilio;
+using Twilio.Base;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Rest.Api.V2010.Account.AvailablePhoneNumberCountry;
+
+namespace Twilio.logic
+{
+   public class Request
+    {
+        //On live Server
+        //string _accountSId = Environment.GetEnvironmentVariable("AC8809a2377cbaaa2ebc24e47f1e0a5d2e").ToString();
+        //string _authToken = Environment.GetEnvironmentVariable("bc7a9de7d3b09e782a72a8ada4cb2287");
+
+        //On local
+        string _accountSId = "AC8809a2377cbaaa2ebc24e47f1e0a5d2e";
+        string _authToken = "bc7a9de7d3b09e782a72a8ada4cb2287";
+
+
+        public Request()
+        {
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            TwilioClient.Init(_accountSId, _authToken);
+        } 
+        public MessageResource SendSMS(string Body,string ToMobileNumber)
+        {
+
+            MessageResource message = MessageResource.Create(
+                body: Body,
+                from: new Twilio.Types.PhoneNumber("+13025261515"),
+                to: new Twilio.Types.PhoneNumber(ToMobileNumber)
+           ); ;
+
+            return message;
+
+        }
+
+        public AvailablePhoneNumberCountryResource FetchNumberBySpecificCountry(Country _country)
+        {
+            AvailablePhoneNumberCountryResource availablePhoneNumberCountry = AvailablePhoneNumberCountryResource.Fetch(pathCountryCode: _country.ToString());
+            return availablePhoneNumberCountry;
+        }
+        public ResourceSet<LocalResource> FindAvailableLocalPhoneNumbersByAreaCode(CountryCode _code, Country _pathCountryCode,int _limit )
+        {
+            ResourceSet<LocalResource> _local = LocalResource.Read(areaCode:(int)_code, pathCountryCode:_pathCountryCode.ToString(), limit: _limit); 
+            return _local; 
+        }
+
+        public IncomingPhoneNumberResource CreateIncomingPhoneNumber(string _PhoneNumber)
+        {
+            IncomingPhoneNumberResource _incomingPhoneNumber = IncomingPhoneNumberResource.Create(phoneNumber: new Twilio.Types.PhoneNumber("_PhoneNumber"));
+            return _incomingPhoneNumber;
+        } 
+    }
+}
